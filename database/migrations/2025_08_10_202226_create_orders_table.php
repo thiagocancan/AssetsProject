db67eade6f309f9f001b3b5c40394f5f8cb8b1cd
Thiago <thiago17@windowslive.com>
2025-08-19 13:02:37 -0300

    feat: implement OrderController with CRUD operations for orders

--- database/migrations/2025_08_10_202226_create_orders_table.php
+++ database/migrations/2025_08_10_202226_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('buyer_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->decimal('total', 10, 2);
             $table->string('status')->default('pending'); // 'pending', 'completed', 'cancelled'
             $table->timestamps();
