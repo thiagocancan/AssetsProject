3d473d96e39f0c0612560c9f564cea040e7c254e
Thiago <thiago17@windowslive.com>
2025-08-29 20:26:14 -0300

    add pagination to assets and review in profile page

--- app/Livewire/ProfilePage.php
+++ app/Livewire/ProfilePage.php
@@ -3,31 +3,55 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\User;
 use App\Models\Asset;
+use App\Models\Review;
 
 class ProfilePage extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'tailwind';
+
+    // Livewire precisa saber que existem 2 paginadores diferentes
+    protected $paginationComponentNames = [
+        'assets' => 'page_assets',
+        'reviews' => 'page_reviews',
+    ];
+
     public $user;
-    public $assets;
+    public $assets_count;
+    public $reviews_count;
+    public $activeTab = 'assets';
 
     public function mount()
     {
-        $this->user = User::with('profile')->find(request()->route('user_id'));
+        $this->user = User::with('profile')->findOrFail(request()->route('user_id'));
 
-        $this->assets = Asset::with('reviews')->where('user_id', request()->route('user_id'))->get();
+        $this->assets_count = Asset::where('user_id', request()->route('user_id'))->count();
 
+        $this->reviews_count = Review::whereHas('asset', function ($q) {
+            $q->where('user_id', $this->user->id);
+        })->count();
     }
 
     public function render()
     {
-        return view('livewire.profile.profile-page',[
-            'user' => $this->user,
-            'assets' => $this->assets,
-        ]) ->layout('components.layouts.base');
-    }
+        $assets = Asset::latest()
+            ->where('user_id', $this->user->id)
+            ->paginate(20, pageName: 'page_assets');
 
-    public $activeTab = 'assets';
+        $reviews = Review::latest()
+            ->with('asset', 'user')
+            ->whereHas('asset', fn ($q) => $q->where('user_id', $this->user->id))
+            ->paginate(5, pageName: 'page_reviews');
+
+        return view('livewire.profile.profile-page', [
+            'assets' => $assets,
+            'reviews' => $reviews,
+        ])->layout('components.layouts.base');
+    }
 
     public function setTab($tab)
     {
--- resources/views/livewire/profile/profile-page.blade.php
+++ resources/views/livewire/profile/profile-page.blade.php
@@ -15,76 +15,77 @@
         </div>
     </div>
 
-    <div class="mb-6 w-fit ml-3 text-zinc-500">
-        <ul class="flex items-center space-x-6">
-            <li>
-                <button 
-                    wire:click="setTab('assets')" 
-                    class="hover:text-black cursor-pointer border-black 
-                        @if($activeTab === 'assets') border-b text-black @endif">
-                    All Assets ({{ $assets->count() }})
-                </button>
-            </li>
-            <li>
-                <button 
-                    wire:click="setTab('reviews')" 
-                    class="hover:text-black cursor-pointer border-black
-                        @if($activeTab === 'reviews') border-b text-black @endif">Reviews ({{ $assets->sum(fn($asset) => $asset->reviews->count()) }})</button>
-            </li>
-        </ul>
-    </div>
+    <div>
+        <div class="mb-6 w-fit ml-3 text-zinc-500 space-x-2">
+            <button 
+                wire:click="setTab('assets')" 
+                class="hover:text-black cursor-pointer border-black 
+                    @if($activeTab === 'assets') border-b text-black @endif">
+                All Assets ({{ $assets_count }})
+            </button>
+            <button 
+                wire:click="setTab('reviews')" 
+                class="hover:text-black cursor-pointer border-black
+                    @if($activeTab === 'reviews') border-b text-black @endif">Reviews ({{ $reviews_count }})
+            </button>
+        </div>
 
-    @if($activeTab === 'assets')
-        <div class="grid grid-cols-4 gap-6">
-            @foreach($assets as $asset)
-                <div class="shadow-md">
-                    <div class="h-50 flex flex-col justify-between p-4 bg-white dark:bg-gray-800" 
-                        style="background-image: url('{{ asset('storage/' . $asset->preview_path) }}'); 
-                        background-size: cover; background-position: center;">
-                        <span class="bg-zinc-800 w-fit px-2 py-1 rounded text-sm text-accent-foreground">{{ $asset->type }}</span>
-                        @if($asset->price  == 0)
-                            <span class="self-end bg-green-300 w-fit px-2 py-1 text-sm">Free</span>
-                        @else
-                            <span class="self-end bg-orange-600/90 w-fit px-2 py-1 text-sm text-amber-50">R$ {{ number_format($asset->price, 2, ',', '.') }}</span>
-                        @endif
-                    </div>
-                    <div class="p-4 bg-white dark:bg-gray-800 flex flex-col gap-2 h-50">
-                        <h3 class="text-lg font-semibold">{{ $asset->title }}</h3>
-                        <p class="text-zinc-500 font-normal text-sm truncate">{{ $asset->description }}</p>
-                        <div class="flex items-center justify-between text-zinc-500 font-normal text-sm">
-                            <div class="flex items-center gap-2">
-                                <div class="flex items-center justify-center text-amber-50 text-sm w-[24px] h-[24px] rounded-full bg-orange-500">JD</div>
-                                <span class="text-zinc-500 font-normal text-sm">{{ $asset->user->name }}</span>
+        @if($activeTab === 'assets')
+            <div class="grid grid-cols-4 gap-6">
+                @foreach($assets as $asset)
+                    <div class="shadow-md">
+                        <div class="h-50 flex flex-col justify-between p-4 bg-white dark:bg-gray-800" 
+                            style="background-image: url('{{ asset('storage/' . $asset->preview_path) }}'); 
+                            background-size: cover; background-position: center;">
+                            <span class="bg-zinc-800 w-fit px-2 py-1 rounded text-sm text-accent-foreground">{{ $asset->type }}</span>
+                            @if($asset->price  == 0)
+                                <span class="self-end bg-green-300 w-fit px-2 py-1 text-sm">Free</span>
+                            @else
+                                <span class="self-end bg-orange-600/90 w-fit px-2 py-1 text-sm text-amber-50">R$ {{ number_format($asset->price, 2, ',', '.') }}</span>
+                            @endif
+                        </div>
+                        <div class="p-4 bg-white dark:bg-gray-800 flex flex-col gap-2 h-50">
+                            <h3 class="text-lg font-semibold">{{ $asset->title }}</h3>
+                            <p class="text-zinc-500 font-normal text-sm truncate">{{ $asset->description }}</p>
+                            <div class="flex items-center justify-between text-zinc-500 font-normal text-sm">
+                                <div class="flex items-center gap-2">
+                                    <div class="flex items-center justify-center text-amber-50 text-sm w-[24px] h-[24px] rounded-full bg-orange-500">JD</div>
+                                    <span class="text-zinc-500 font-normal text-sm">{{ $asset->user->name }}</span>
+                                </div>
+                                <div class="text-zinc-500 font-normal text-sm">🌟 4.8</div>
                             </div>
-                            <div class="text-zinc-500 font-normal text-sm">🌟 4.8</div>
+                            <div class="flex gap-2 mt-auto">
+                                <a class="text-sm text-amber-50 bg-orange-600/90 py-1 px-3" href="{{ route('assets.asset-page', $asset->id) }}">Details</a>
+                                <button class="py-1 px-3 border ">♥</button>
+                            </div>
                         </div>
-                        <div class="flex gap-2 mt-auto">
-                            <a class="text-sm text-amber-50 bg-orange-600/90 py-1 px-3" href="{{ route('assets.asset-page', $asset->id) }}">Details</a>
-                            <button class="py-1 px-3 border ">♥</button>
+                    </div>
+                @endforeach
+            </div>
+            <div class="mt-4">
+                {{ $assets->links() }}
+            </div>
+        @elseif($activeTab === 'reviews')
+            <div class="space-y-3">
+                @foreach($reviews as $review)
+                    <div class="border border-zinc-300 rounded p-6 space-y-4">
+                        <div class="flex justify-between">
+                            <h2 class="font-semibold text-lg">{{ $review->asset->title }}</h2>
+                            @for ($i = 0; $i < $review->rating; $i++)
+                                ⭐
+                            @endfor
+                        </div>
+                            <p class="text-sm text-zinc-700">{{ $review->comment }}</p>
+                        <div class="flex items-center space-x-3">
+                            <img class="rounded-full size-6" src="https://ui-avatars.com/api/?name={{ urlencode($review->user->name) }}&background=random" alt="reviewer name">
+                            <h3 class="text-sm">{{ $review->user->name }} - {{ $review->created_at->diffForHumans() }}</h3>
                         </div>
                     </div>
+                @endforeach
+                <div class="mt-4">
+                    {{ $reviews->links() }}
                 </div>
-            @endforeach
-        </div>
-    @elseif($activeTab === 'reviews')
-        <div class="space-y-3">
-            @foreach($assets as $asset)
-                <div class="border border-zinc-300 rounded p-6 space-y-4">
-                    <div class="flex justify-between">
-                        <h2 class="font-semibold text-lg">{{ $asset->title }}</h2>
-                        @for ($i = 0; $i < $asset->reviews->first()->rating; $i++)
-                            ⭐
-                        @endfor
-                    </div>
-                    @if($asset->reviews->first())
-                        <p class="text-sm text-zinc-700">{{ $asset->reviews->first()->comment }}</p>
-                    @endif
-                    <div class="flex items-center space-x-3">
-                        <img class="rounded-full size-6" src="https://ui-avatars.com/api/?name={{ urlencode($asset->reviews->first()->user->name) }}&background=random" alt="">
-                        <h3 class="text-sm">{{ $asset->reviews->first()->user->name }} - {{ $asset->reviews->first()->created_at->diffForHumans() }}</h3>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    @endif
+            </div>
+        @endif
+    </div>
 </div>
